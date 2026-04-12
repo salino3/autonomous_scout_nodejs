b849b2fcb5d67518fc0762ec7b74d5ae36544bc0
Salino3 <flaviobcn012@gmail.com>
2026-04-12 10:26:08 +0200

    signup

---
+++ src/controllers/auth-controller.ts
@@ -0,0 +1,32 @@
+import { type Request, type Response } from "express";
+import { AuthUserService } from "../services/auth-service.js";
+
+const authService = new AuthUserService();
+
+export class AuthController {
+  static async signup(req: Request, res: Response) {
+    try {
+      const { username, email, password, role } = req.body;
+
+      // Basic validation before hitting the service
+      if (!username || !email || !password) {
+        return res
+          .status(400)
+          .json({ error: "Email, username and password are required" });
+      }
+
+      // Pass 'password' here.
+      // Note: Later we will add a hashing step before this!
+      const newUser = await authService.createUser(
+        username,
+        email,
+        password,
+        role,
+      );
+
+      return res.status(201).json(newUser);
+    } catch (error: any) {
+      return res.status(400).json({ error: error.message });
+    }
+  }
+}
