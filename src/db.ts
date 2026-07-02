7ef06f0b228bad52e1c6023fb4cbf97893324df0
Salino3 <flaviobcn012@gmail.com>
2026-07-02 12:12:28 +0200

    fix

--- src/db.ts
+++ src/db.ts
@@ -7,9 +7,10 @@ export const pool = new pg.Pool({
   password: DB_CONFIG.PASSWORD,
   database: DB_CONFIG.DATABASE,
   port: Number(DB_CONFIG.PORT_DB),
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: true }
+      : false,
 });
 
 export const query = (text: string, params?: any[]) => pool.query(text, params);
